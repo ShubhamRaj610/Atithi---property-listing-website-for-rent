fc1fdd6ddf7a1661d9d55eb55bd8704d2a57162f
Shubham <shubham@gmail.com>
2026-06-02 01:42:52 +0530

    Fix: uncomment root route and redirect to listings

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -8,6 +9,7 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const aiRoutes = require("./routes/ai");
 
 
 const wrapAsync = require("./utils/wrapAsync.js");
@@ -22,7 +24,8 @@ const userRouter = require("./routes/user.js");
 
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -33,7 +36,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 // 1. Basic Settings
@@ -43,8 +46,10 @@ app.set("views", path.join(__dirname, "views"));
 
 // 2. Standard Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
+app.use("/ai", aiRoutes);
 
 // 3. Session Configuration 
 const sessionOptions = {
@@ -79,9 +84,12 @@ app.use((req, res, next) => {
 });
 
 // 6. Routes
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
+app.use("/listings", listingRouter);
+app.use("/", userRouter);
 
 // app.get("/demouser", async (req, res) => {
 //   try {
@@ -109,6 +117,7 @@ app.use((req, res, next) => {
 app.use("/listings", listingRouter);
 app.use("/", userRouter); 
 
+
 // 7. Error Handling
 // Use a named parameter (like 'path') with the wildcard
 // Remove the string path entirely. 
@@ -135,135 +144,6 @@ app.listen(8080, () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const Listing = require("./models/listing.js");
-// const path = require("path");
-// const methodOverride = require("method-override");
-// const ejsMate = require("ejs-mate");
-// const wrapAsync = require("./utils/wrapAsync.js");
-// const ExpressError = require("./utils/ExpressError.js");
-// const {listingSchema} = require("./schema.js");
-// const listings = require("./routes/listing.js");
-// const session = require("express-session");
-// const passport = require("passport");
-// const LocalStrategy = require("passport-local");
-// const User = require("./models/user.js");
-// const flash = require("connect-flash");
-
-// const listingRouter = require("./routes/listing.js");
-// const userRouter = require("./routes/user.js");
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
-// main()
-//   .then(() => {
-//     console.log("connected to DB");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// async function main() {
-//   await mongoose.connect(MONGO_URL);
-// }
-
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(methodOverride("_method"));
-// app.engine("ejs", ejsMate);
-// app.use(express.static(path.join(__dirname,"/public")));
-
-// const sessionOptions = {
-//   secret : "mysupersecretcode",
-//   resave : false,
-//   saveUninitialized : true,
-//   cookie:{
-//     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-//     maxAge :7 * 24 * 60 * 60 * 1000,
-//     httpOnly : true,
-//   },
-// };
-
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
-
-
-
-// app.use("/listings",listings);
-
-// app.use(session(sessionOptions));
-// app.use(flash());
-
-
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-
-// /* ✅ ADDED MIDDLEWARE (only this part) */
-// app.use((req,res,next)=>{
-//   res.locals.success = req.flash("success");
-//   res.locals.error = req.flash("error");
-//   res.locals.currUser = req.user;
-//   next();
-// });
-
-// app.get("/demouser", async (req, res, next) => {
-//   try {
-//     const fakeUser = new User({
-//       email: "raman10@gmail.com",
-//       username: "Raman10",
-//     });
-
-//     const registeredUser = await User.register(fakeUser, "Hello");
-//     res.send(registeredUser);
-
-//   } catch (err) {
-//     res.send(err.message);
-//   }
-// });
-
-
-
-// app.use("/listings",listingRouter);
-// app.use("/",userRouter);
-
-// // app.use((err,req,res,next)=>{
-// //   console.log(err);
-// //   res.send(err.message);
-// // });
-
-// app.use((err, req, res, next) => {
-//     let { status = 500, message = "Something went wrong" } = err;
-//     res.status(status).render("error.ejs", { err });
-// });
-
-// app.listen(8080, () => {
-//   console.log("server is listening to port 8080");
-// });
 
 
 
