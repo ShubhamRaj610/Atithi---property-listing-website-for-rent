0b31230e4275fd997f34287694bfd363c172ada5
Shubham <shubham@gmail.com>
2026-06-02 15:00:01 +0530

    Fixing schema

--- schema.js
+++ schema.js
@@ -1,16 +1,14 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
-
-    listing : Joi.object({
-
-        title : Joi.string().required(),
-        description : Joi.string().required(),
-        price : Joi.number().required().min(0),
-        image : Joi.string().allow("" , null),
-        location : Joi.string().required(),
-        country : Joi.string().required(),
-
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.object({
+            url: Joi.string().allow("", null)
+        })
     }).required()
-
-})
+});
